cc5c6d7feafcb933a5b445de9e0669c0e7d8af28
root <root@ip-172-31-10-156.ap-south-1.compute.internal>
2025-02-05 06:04:32 +0000

    my second commit

---
+++ priya.sh
@@ -0,0 +1,12 @@
+def greet():
+    message = "Hello, World!"
+        return message
+
+	def print_greeting(times):
+	    for _ in range(times):
+		            print(greet())
+
+			    if __name__ == "__main__":
+				        times = int(input("How many times to print 'Hello, World!'? "))
+					    print_greeting(times)
+
